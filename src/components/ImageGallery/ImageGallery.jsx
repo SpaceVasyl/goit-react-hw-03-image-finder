810b47324cc2f127c1a8610d6de2a303975ee966
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-04-20 22:05:45 +0300

    qw

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css'
 import PropTypes from "prop-types";
+import { nanoid } from 'nanoid';
 
 export const ImageGallery = ({image , imageModal}) =>  {
     return ( 
       <><ul className={css.ImageGallery}>
-        {image.map((item, index) => <ImageGalleryItem item={item} key={item.id + index} imageModal={imageModal} />)}  
+        {image.map((item, index) => <ImageGalleryItem item={item} key={nanoid()} imageModal={imageModal} />)}  
       </ul>
       </>
     );
